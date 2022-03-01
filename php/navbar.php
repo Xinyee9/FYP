e4fe1c1572d377baa1d757954905ee390249d1f4
Xin yee <xinyeee926@gmail.com>
2022-03-01 22:41:45 +0800

    changes

--- php/navbar.php
+++ php/navbar.php
@@ -4,7 +4,7 @@
 ?>
 
 <div class="topnav">
-    <a href="#">HOME</a>
+    <a href="index.php">HOME</a>
     <a href="menu.php">MENU</a>
     <?php
     if (isset($_SESSION['logged']) && $_SESSION['logged'] == 1 && $_SESSION['privilege'] == "user") {
@@ -14,7 +14,6 @@
         <a href="" style="float: right;">ORDER HISTORY</a>
         <a href="" style="float: right;">EDIT PROFILE</a>
         <a style="-webkit-user-select: none; cursor: default;">Welcome, ' . $_SESSION['username'] . '</a>';
-
     } else if (isset($_SESSION['logged']) && $_SESSION['logged'] == 1 && $_SESSION['privilege'] == "admin") {
         echo '
         <a href="./php/logout.php" style="float: right;">LOGOUT</a>
@@ -22,7 +21,6 @@
         <a href="" style="float: right;">ORDER MANAGEMENT</a>
         <a href="./admin/adminpanel.php" style="float: right;">ADMIN PANEL</a>
         <a style="-webkit-user-select: none; cursor: default;">Welcome, ' . $_SESSION['username'] . '</a>';
-        
     } else {
         echo '
         <a href="aboutus.php" style="float: right;">ABOUT US</a>
