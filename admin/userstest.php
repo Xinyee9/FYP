ab5ab5c9131e2237e02e82746452870bc72c2c56
Wee woen <ngweewoen0415@gmail.com>
2022-04-05 02:31:01 +0800

    c

--- admin/userstest.php
+++ admin/userstest.php
@@ -86,12 +86,7 @@ include('config/constants.php'); ?>
         <div class="main">
             <div class="topbar">
                 <div class="toggle " onclick="toggleMenu();"></div>
-                <div class="search">
-                    <label>
-                        <input type="text" placeholder="Search here">
-                        <i class="fa fa-search" aria-hidden="true"></i>
-                    </label>
-                </div>
+                
                 <div class="user">
                     <img src="user.jpg">
                 </div>
