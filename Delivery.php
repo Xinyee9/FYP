f4473416329684b1f6aa943a9b764f70b6a58338
Jayson1314 <jaysonlimck520@gmail.com>
2022-04-06 13:54:22 +0800

    changes

--- Delivery.php
+++ Delivery.php
@@ -1,6 +1,8 @@
 <?php
 	require_once('./php/dbconnect.php');
 	session_start();
+
+	header("refresh: 4");
 ?>
 
 <!DOCTYPE html>
@@ -53,6 +55,181 @@
 			color: #5E6977;
 			font-size: 30px;
 			font-weight: 400;
+			color: rgb(207, 207, 207);
+			color: #ffe6ff;
+			text-shadow: 0 0 0.6rem #ffe6ff, 0 0 1.5rem #ff65bd,
+				-0.2rem 0.1rem 1rem #ff65bd, 0.2rem 0.1rem 1rem #ff65bd,
+				0 -0.5rem 2rem #ff2483, 0 0.5rem 3rem #ff2483;
+			animation: shine 2s forwards, flicker 2s infinite;
+		}
+
+		@keyframes blink {
+			0%,
+			22%,
+			36%,
+			75% {
+				color: #ffe6ff;
+				text-shadow: 0 0 0.6rem #ffe6ff, 0 0 1.5rem #ff65bd,
+				-0.2rem 0.1rem 1rem #ff65bd, 0.2rem 0.1rem 1rem #ff65bd,
+				0 -0.5rem 2rem #ff2483, 0 0.5rem 3rem #ff2483;
+			}
+			28%,
+			33% {
+				color: #ff65bd;
+				text-shadow: none;
+			}
+			82%,
+			97% {
+				color: #ff2483;
+				text-shadow: none;
+			}
+		}
+
+		.flicker {
+		animation: shine 2s forwards, blink 3s 2s infinite;
+		}
+
+		.fast-flicker {
+		animation: shine 2s forwards, blink 2s 1s infinite;
+		}
+
+		@keyframes shine {
+			0% {
+				color: #6b1839;
+				text-shadow: none;
+			}
+			100% {
+				color: #ffe6ff;
+				text-shadow: 0 0 0.6rem #ffe6ff, 0 0 1.5rem #ff65bd,
+				-0.2rem 0.1rem 1rem #ff65bd, 0.2rem 0.1rem 1rem #ff65bd,
+				0 -0.5rem 2rem #ff2483, 0 0.5rem 3rem #ff2483;
+			}
+		}
+
+		@keyframes flicker {
+			from {
+				opacity: 1;
+			}
+
+			4% {
+				opacity: 0.9;
+			}
+
+			6% {
+				opacity: 0.85;
+			}
+
+			8% {
+				opacity: 0.95;
+			}
+
+			10% {
+				opacity: 0.9;
+			}
+
+			11% {
+				opacity: 0.922;
+			}
+
+			12% {
+				opacity: 0.9;
+			}
+
+			14% {
+				opacity: 0.95;
+			}
+
+			16% {
+				opacity: 0.98;
+			}
+
+			17% {
+				opacity: 0.9;
+			}
+
+			19% {
+				opacity: 0.93;
+			}
+
+			20% {
+				opacity: 0.99;
+			}
+
+			24% {
+				opacity: 1;
+			}
+
+			26% {
+				opacity: 0.94;
+			}
+
+			28% {
+				opacity: 0.98;
+			}
+
+			37% {
+				opacity: 0.93;
+			}
+
+			38% {
+				opacity: 0.5;
+			}
+
+			39% {
+				opacity: 0.96;
+			}
+
+			42% {
+				opacity: 1;
+			}
+
+			44% {
+				opacity: 0.97;
+			}
+
+			46% {
+				opacity: 0.94;
+			}
+
+			56% {
+				opacity: 0.9;
+			}
+
+			58% {
+				opacity: 0.9;
+			}
+
+			60% {
+				opacity: 0.99;
+			}
+
+			68% {
+				opacity: 1;
+			}
+
+			70% {
+				opacity: 0.9;
+			}
+
+			72% {
+				opacity: 0.95;
+			}
+
+			93% {
+				opacity: 0.93;
+			}
+
+			95% {
+				opacity: 0.95;
+			}
+
+			97% {
+				opacity: 0.93;
+			}
+
+			to {
+				opacity: 1;
+			}
 		}
 
 		.status {
@@ -93,13 +270,15 @@
 		}
 
 		.container {
-            background-color: rgba(255, 255, 255, .7);
+			margin-left: 200px;
+            background-color: rgba(255, 255, 255, .8);
             padding: 5px 20px 15px 20px;
             border: 1px solid lightgrey;
             border-radius: 3px;
 			margin-top: -50px;
 			font-family: "Times New Roman", Times, serif;
 			font-size: 16px;
+  			width: 100%;
         }
 
 		.total {
@@ -200,7 +379,7 @@
 								?>
 								<?php echo '<pre>';
 									echo '<div class = "details">';
-									echo "\t\t\t$qty\t\t\t\t","RM ".number_format($oriprice, 2)."\t\t\t","RM ".number_format($subtotal, 2)."";
+									echo "\t\t\t$qty\t\t\t\t","RM ".number_format($oriprice, 2)."\t\t\t\t","RM ".number_format($subtotal, 2)."";
 									echo '</div>';
 									echo '</pre>';
 								?>
